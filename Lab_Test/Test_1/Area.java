3863f904e6d9ae8c829f2cb158025c5c1c9d4789
nxtsourav7 <nxtsourav7@gmail.com>
2024-07-10 22:47:03 +0600

    change

--- Lab_Test/Test_1/Area.java
+++ Lab_Test/Test_1/Area.java
@@ -1,7 +1,12 @@
 package Lab_Test.Test_1;
 
 public class Area {
+    int length, width;
     Area(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+    public void result() {
         System.out.println("Area : " + length * width);
     }
 }
--- Lab_Test/Test_1/Main.java
+++ Lab_Test/Test_1/Main.java
@@ -3,5 +3,6 @@ package Lab_Test.Test_1;
 public class Main {
     public static void main(String[] args) {
         Area a1 = new Area(600, 400);
+        a1.result();
     }
 }
