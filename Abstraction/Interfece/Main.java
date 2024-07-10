dd267708e79b669ce1f5ceffefa882aec7a124b2
nxtsourav7 <nxtsourav7@gmail.com>
2024-07-10 23:20:30 +0600

    change

--- Abstraction/Interfece/Main.java
+++ Abstraction/Interfece/Main.java
@@ -3,11 +3,11 @@ package Abstraction.Interfece;
 public class Main {
     public static void main(String[] args) {
         // Calculate the area of Circle
-        Circle c1 = new Circle(10);
-        System.out.println(c1.calculateArea());
+        Shape c1 = new Circle(10);
+        System.out.println("Area of Circle : " + c1.calculateArea());
 
         // Calculate the area of Rectangle
-        Rectangle r1 = new Rectangle(15, 10);
-        System.out.println(r1.calculateArea());
+        Shape r1 = new Rectangle(15, 10);
+        System.out.println("Area of Rectangle : " + r1.calculateArea());
     }
 }
