a93de443743a9b911e23208db1801df477bbef44
nxtsourav7 <nxtsourav7@gmail.com>
2024-07-10 22:40:30 +0600

    add lab 1

---
+++ Lab_Test/Test_1/Area.java
@@ -0,0 +1,7 @@
+package Lab_Test.Test_1;
+
+public class Area {
+    Area(int length, int width) {
+        System.out.println("Area : " + length * width);
+    }
+}
---
+++ Lab_Test/Test_1/Main.java
@@ -0,0 +1,7 @@
+package Lab_Test.Test_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Area a1 = new Area(600, 400);
+    }
+}
